1cf0b9ca555e93554857b3a7eb04a1b7e27c8b46
scifisatan <brawlershere@gmail.com>
2024-07-26 18:44:41 +0545

    auth plus user connected to images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,39 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
-  const images = await db.query.images.findMany(
-    {
-      orderBy: (model, { desc }) => desc(model.id),
-    });
+async function Images() {
+
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
   return (
-    <main>
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48 flex flex-col">
-            <img src={image.url} />
-            <div>{image.name}</div>
-          </div>))
-        }
-      </div>
-    </main>
-  );
+    <div className="flex flex-wrap gap-4">
+      {images.map((image, index) => (
+        <div key={image.id + "-" + index} className="w-48 flex flex-col">
+          <img src={image.url} />
+          <div>{image.name}</div>
+        </div>))
+      }
+    </div>
+  )
+}
+
+export default async function HomePage() {
+  {
+    return (
+      <main>
+        <SignedOut>
+          <div className="w-full h-full text-2xl text-center">
+            Please sign in to get the images
+          </div>
+        </SignedOut>
+        <SignedIn>
+          <Images />
+        </SignedIn>
+
+      </main>
+    );
+  }
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
-
-export default {
+import { withUt } from "uploadthing/tw"
+export default withUt({
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
@@ -11,4 +11,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+}) satisfies Config;
