3ca4ecbb885ad9ce7da0e7bce0a14883cf481729
scifisatan <brawlershere@gmail.com>
2024-07-26 12:23:35 +0545

    project setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,27 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-x1 font-semibold">
+      <div>
+        Gallery
+      </div>
+      <div>
+        Sign In
+      </div>
+    </nav>
+  )
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+    <html lang="en" className={`${GeistSans.variable} flex flex-col gap-4`}>
+      <body>
+        <TopNav />
+        {children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+const mockUrls = [
+  "https://utfs.io/f/672474ba-eaa0-4204-9c32-f231eec12221-ajuinb.jpg",
+  "https://utfs.io/f/ab62b4ff-3fd1-42a4-8cfb-04b24aefb3b3-ealxc4.jpg"
+]
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}))
+
 export default function HomePage() {
   return (
-    <>
-      Hello World
-    </>
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>))
+        }
+
+      </div>
+    </main>
   );
 }
