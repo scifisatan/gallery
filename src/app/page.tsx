52a2f2f902ae504ae5f72655ca69eecfeaeb53f0
scifisatan <brawlershere@gmail.com>
2024-07-26 17:53:41 +0545

    clerk added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,21 @@
-// const mockUrls = [
-//   "https://utfs.io/f/672474ba-eaa0-4204-9c32-f231eec12221-ajuinb.jpg",
-//   "https://utfs.io/f/ab62b4ff-3fd1-42a4-8cfb-04b24aefb3b3-ealxc4.jpg"
-// ]
-
-// const mockImages = mockUrls.map((url, index) => ({
-//   id: index + 1,
-//   url,
-// }))
-
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const images = await db.query.images.findMany();
+  const images = await db.query.images.findMany(
+    {
+      orderBy: (model, { desc }) => desc(model.id),
+    });
   return (
     <main>
       <div className="flex flex-wrap gap-4">
         {[...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
+          <div key={image.id + "-" + index} className="w-48 flex flex-col">
             <img src={image.url} />
+            <div>{image.name}</div>
           </div>))
         }
-
       </div>
     </main>
   );
