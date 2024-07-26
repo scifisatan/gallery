26b4300305f83257835e0df432259c3a073eab23
scifisatan <brawlershere@gmail.com>
2024-07-26 12:55:34 +0545

    database integrated with ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,24 @@
-const mockUrls = [
-  "https://utfs.io/f/672474ba-eaa0-4204-9c32-f231eec12221-ajuinb.jpg",
-  "https://utfs.io/f/ab62b4ff-3fd1-42a4-8cfb-04b24aefb3b3-ealxc4.jpg"
-]
+// const mockUrls = [
+//   "https://utfs.io/f/672474ba-eaa0-4204-9c32-f231eec12221-ajuinb.jpg",
+//   "https://utfs.io/f/ab62b4ff-3fd1-42a4-8cfb-04b24aefb3b3-ealxc4.jpg"
+// ]
 
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}))
+// const mockImages = mockUrls.map((url, index) => ({
+//   id: index + 1,
+//   url,
+// }))
+
+import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const images = await db.query.images.findMany();
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {mockImages.map((image) => (
-          <div key={image.id} className="w-48">
+        {[...images, ...images, ...images].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} />
           </div>))
         }
